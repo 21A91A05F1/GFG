d489dfd98e56435650ab3ebcfed0578fcba8f34d
Naga Sathwika Kovvuri <97424376+21A91A05F1@users.noreply.github.com>
2024-03-05 02:17:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Index/maximum-index.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int A[], int N) 
+    { 
+        // Your code here
+        int i,j,count=0;
+        for( i=0;i<N;i++)
+        {
+            
+            for( j=N-1;j>=i;j--)
+            {
+                if(A[i]<=A[j] )
+                {
+                    count=max(count,j-i);
+                    break;
+                }
+                
+            }
+            
+        }
+         return count;
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+} 
+// } Driver Code Ends
