2c80cecfbfd3ca7a0d2f6874d739ffd2c25809bf
Naga Sathwika Kovvuri <97424376+21A91A05F1@users.noreply.github.com>
2024-07-24 12:05:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(int n, int arr[]) {
+        // Code here
+        stack<int>st;
+        vector<int>ve;
+        st.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--)
+        {
+            if(arr[i]>=st.top()) st.push(arr[i]);
+        }
+        while(!st.empty())
+        {
+            ve.push_back(st.top());
+            st.pop();
+        }
+        return ve;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t; // testcases
+    while (t--) {
+        long long n;
+        cin >> n; // total size of array
+
+        int arr[n];
+
+        // inserting elements in the array
+        for (long long i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        // calling leaders() function
+        vector<int> v = obj.leaders(n, arr);
+
+        // printing elements of the vector
+        for (auto it = v.begin(); it != v.end(); it++) {
+            cout << *it << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
